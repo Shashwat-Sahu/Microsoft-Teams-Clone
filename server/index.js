eef0912126a0d42816a1cadb9442bd56c40c30bb
Shashwat-Sahu <shashatsahu25@gmail.com>
2021-06-19 01:57:57 +0530

    meet changes

---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const cors = require("cors");
+app.use(cors)
+const io = socket(server,{
+    cors: {
+        origin: '*',
+      }
+});
+require ("dotenv").config();
+const path = require("path");
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+    socket.on("join room", ({roomID,options}) => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 100) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push({id:socket.id,options});
+        } else {
+            users[roomID] = [{id:socket.id,options}];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(user => user.id !== socket.id);
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID,options:payload.options });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id,options:payload.options });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(user => user.id !== socket.id);
+            users[roomID] = room;
+        }
+        socket.broadcast.emit('user left',socket.id)
+    });
+
+    socket.on('change', (payload) => {
+        // console.log(payload)
+        const index = users[socketToRoom[socket.id]].findIndex(user=>user.id === socket.id)
+        users[socketToRoom[socket.id]][index].options={video:payload.video,audio:payload.audio};
+        // console.log(JSON.stringify(users))
+        socket.broadcast.emit('change',payload)
+    });
+
+});
+
+app.get("/",(req,res)=>{
+    res.send("Live")
+});
+const PORT = process.env.PORT || 8000;
+// if(process.env.PROD){
+//     app.use( express.static(__dirname + '/client/build'));
+//     app.get('*', (request, response) => {
+// 	    response.sendFile(path.join(__dirname, 'client/build/index.html'));
+//     });
+// }
+server.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`)}
+    )
