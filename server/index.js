a956940605f894dad538d42473035ad1f39b21e6
Shashwat-Sahu <shashatsahu25@gmail.com>
2021-06-28 01:21:58 +0530

    server changes

--- server/index.js
+++ server/index.js
@@ -5,31 +5,31 @@ const server = http.createServer(app);
 const socket = require("socket.io");
 const cors = require("cors");
 app.use(cors)
-const io = socket(server,{
+const io = socket(server, {
     cors: {
         origin: '*',
-      }
+    }
 });
-require ("dotenv").config();
+require("dotenv").config();
 const path = require("path");
 
 const users = {};
 
 const socketToRoom = {};
 
-const screenShareInRoom ={};
+const screenShareInRoom = {};
 
 io.on('connection', socket => {
-    socket.on("join room", ({roomID,options,name}) => {
+    socket.on("join room", ({ roomID, options, name }) => {
         if (users[roomID]) {
             const length = users[roomID].length;
             if (length === 15) {
                 socket.emit("room full");
                 return;
             }
-            users[roomID].push({id:socket.id,options,name});
+            users[roomID].push({ id: socket.id, options, name });
         } else {
-            users[roomID] = [{id:socket.id,options,name}];
+            users[roomID] = [{ id: socket.id, options, name }];
         }
         socketToRoom[socket.id] = roomID;
         const usersInThisRoom = users[roomID].filter(user => user.id !== socket.id);
@@ -37,44 +37,45 @@ io.on('connection', socket => {
     });
 
     socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID,options:payload.options,name:payload.name });
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID, options: payload.options, name: payload.name });
     });
 
     socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id,options:payload.options,name:payload.name });
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id, options: payload.options, name: payload.name });
     });
 
-    
+
     socket.on('disconnectMeet', () => {
 
         const roomID = socketToRoom[socket.id];
+        delete socketToRoom[socket.id];
         console.log(`User ${socket.id} left from room: ${roomID}`);
         let room = users[roomID];
         if (room) {
             room = room.filter(user => user.id !== socket.id);
             users[roomID] = room;
         }
-        users[roomID].forEach(user=>{
-            if(socket.id!==user.id){
-            io.to(user.id).emit('user left',socket.id)
-            io.to(user.id).emit('user left screen stream',socket.id+"-screen-share")
+        users[roomID].forEach(user => {
+            if (socket.id !== user.id) {
+                io.to(user.id).emit('user left', socket.id)
+                io.to(user.id).emit('user left screen stream', socket.id + "-screen-share")
             }
         })
 
-        
+
     });
 
     socket.on('change', (payload) => {
-        
-        const index = users[socketToRoom[socket.id]].findIndex(user=>user.id === socket.id)
-        users[socketToRoom[socket.id]][index].options={video:payload.video,audio:payload.audio};
-        
-        socket.broadcast.emit('change',payload)
+
+        const index = users[socketToRoom[socket.id]].findIndex(user => user.id === socket.id)
+        users[socketToRoom[socket.id]][index].options = { video: payload.video, audio: payload.audio };
+
+        socket.broadcast.emit('change', payload)
     });
-    socket.on("send message",(payload)=>{
-        users[payload.roomID].forEach(user=>{
-            if(socket.id!==user.id)
-            io.to(user.id).emit('receivedMessage',payload)
+    socket.on("send message", (payload) => {
+        users[payload.roomID].forEach(user => {
+            if (socket.id !== user.id)
+                io.to(user.id).emit('receivedMessage', payload)
         })
 
     })
@@ -85,34 +86,42 @@ io.on('connection', socket => {
     });
 
     socket.on("returning screen stream", payload => {
-        io.to(payload.callerID).emit('receiving returned screen stream', { signal: payload.signal, id: socket.id+"-screen-share" });
+        io.to(payload.callerID).emit('receiving returned screen stream', { signal: payload.signal, id: socket.id + "-screen-share" });
     });
 
-    socket.on("screen stream update",(payload)=>{
+    socket.on("screen stream update", (payload) => {
 
-        if(payload.updateStream)
-        {
-            screenShareInRoom[payload.roomID] = socket.id;
+        if (payload.updateStream) {
+            screenShareInRoom[payload.roomID] = {id:socket.id,name:payload.name};
         }
-        else
-        {
-            screenShareInRoom[payload.roomID] = null;
+        else {
+            delete screenShareInRoom[payload.roomID] 
         }
-        users[payload.roomID].forEach(user=>{
-            if(socket.id!==user.id)
-            io.to(user.id).emit('screen share update',{updateStream:payload.updateStream,id:socket.id});
+        users[payload.roomID].forEach(user => {
+            if (socket.id !== user.id)
+                io.to(user.id).emit('screen share update', 
+                { 
+                    updateStream: payload.updateStream, 
+                    id: socket.id, 
+                    name: payload.name 
+                });
         })
 
     })
-    socket.on("screen streaming running for new user",(payload)=>{
+    socket.on("screen streaming running for new user", (payload) => {
         console.log(screenShareInRoom)
-        if(screenShareInRoom[payload.roomID])
-        io.to(socket.id).emit('screen share update',{updateStream:true,id:screenShareInRoom[payload.roomID]})
+        if (screenShareInRoom[payload.roomID])
+            io.to(socket.id).emit('screen share update', 
+            { 
+                updateStream: true, 
+                id: screenShareInRoom[payload.roomID].id,
+                name: screenShareInRoom[payload.roomID].name 
+            })
     })
 
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Live")
 });
 const PORT = process.env.PORT || 8000;
@@ -122,6 +131,7 @@ const PORT = process.env.PORT || 8000;
 // 	    response.sendFile(path.join(__dirname, 'client/build/index.html'));
 //     });
 // }
-server.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`)}
-    )
+server.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+}
+)
